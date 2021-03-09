2cf8c49af41030f3ee68416161aabb79ab8e2ba1
Ines Nieto <insherondale26@gmail.com>
2021-03-09 18:20:48 +0100

    ejercicio 3 hecho

--- P1/geometria_revolucion/line_pawn.js
+++ P1/geometria_revolucion/line_pawn.js
@@ -9,30 +9,29 @@ class LinePawn extends THREE.Object3D {
     this.points = [];
     this.setPoints();
 
-    var pawnMat = new THREE.MeshPhongMaterial({color: 0x33DDFF });
+    var pawnMat = new THREE.MeshNormalMaterial();
     pawnMat.flatShading = true;
     pawnMat.needsUpdate = true;
-
-    this.pawnGeometry = new THREE.Mesh(new THREE.LatheGeometry(this.points),pawnMat);
-    
-    this.pawnLineGeometry = new THREE.Geometry();
-    this.pawnLineGeometry.vertices = this.points;
-    
-    this.pawn = new THREE.Line(this.pawnLineGeometry, pawnMat);
-
-    
-    this.pawn.position.y = (this.points[this.points.length-1].getComponent(1)-this.points[0].getComponent(1))/2;
+    var pawnGeo = new THREE.LatheGeometry(this.points,this.guiControls.resolution,0,this.guiControls.angle);
+    this.pawn = new THREE.Mesh(pawnGeo,pawnMat);
+  
     // Y añadirlo como hijo del Object3D (el this)
     this.add (this.pawn);
   }
 
   createGUI (gui,titleGui) {
-    // Controles para la modificación de la geometría del cilindro
     this.guiControls = new function () {
       this.resolution = 3;
-      this.angle = 1.0;
+      this.angle = 2*Math.PI;
     } 
     
+    var that = this;
+
+    var folder = gui.addFolder (titleGui);
+    folder.add (this.guiControls, 'resolution', 1, 10, 1).name ('Resolución : ').listen()
+    .onChange(function(value){that.changeGeometry();});
+    folder.add (this.guiControls, 'angle', 0, 2*Math.PI, Math.PI/4).name ('Ángulo : ').listen()
+    .onChange(function(value){that.changeGeometry();});
 
   }
   
@@ -40,18 +39,26 @@ class LinePawn extends THREE.Object3D {
 
   }
 
+  changeGeometry(){
+    var newGeometry = new THREE.LatheGeometry(this.points,this.guiControls.resolution,0,this.guiControls.angle);
+    this.pawn.geometry = newGeometry;
+  }
+
   setPoints(){
-     this.points.push(new THREE.Vector3(1.0, -1.4, 0.0)); 
-     this.points.push(new THREE.Vector3(1.0, -1.1 ,0.0)); 
-     this.points.push(new THREE.Vector3(0.5 ,-0.7, 0.0)); 
-     this.points.push(new THREE.Vector3(0.4 ,-0.4 ,0.0)); 
-     this.points.push(new THREE.Vector3(0.4, 0.5, 0.0)); 
-     this.points.push(new THREE.Vector3(0.5, 0.6 ,0.0)); 
-     this.points.push(new THREE.Vector3(0.3, 0.6, 0.0)); 
-     this.points.push(new THREE.Vector3(0.5, 0.8, 0.0)); 
-     this.points.push(new THREE.Vector3(0.55, 1.0, 0.0)); 
-     this.points.push(new THREE.Vector3(0.5 ,1.2 ,0.0)); 
-     this.points.push(new THREE.Vector3(0.3 ,1.4, 0.0)); 
+     this.points.push(new THREE.Vector3(1.0, -1.4, 0.0));
+     this.points.push(new THREE.Vector3(1.0, -1.1, 0.0));
+     this.points.push(new THREE.Vector3(0.5, -0.7 ,0.0));
+     this.points.push(new THREE.Vector3(0.4, -0.4, 0.0));
+     this.points.push(new THREE.Vector3(0.4, 0.5, 0.0));
+     this.points.push(new THREE.Vector3(0.5, 0.6, 0.0));
+     this.points.push(new THREE.Vector3(0.3, 0.6, 0.0));
+     this.points.push(new THREE.Vector3(0.5, 0.8 ,0.0));
+     this.points.push(new THREE.Vector3(0.55, 1.0, 0.0));
+     this.points.push(new THREE.Vector3(0.5, 1.2, 0.0));
+     this.points.push(new THREE.Vector3(0.3, 1.4, 0.0));
+
+
+
   }
 }
 
